265418bdef6dff66b3970493743b0e3d339544f7
corydagosta001 <69530811+corydagosta001@users.noreply.github.com>
2021-02-16 11:55:32 -0500

    Add files via upload

---
+++ test2/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+namespace test2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public IServiceProvider ServiceProvider { get; private set; }
+
+        public IConfiguration Configuration { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient();
+            services.AddTransient(typeof(MainWindow));
+        }
+    }
+
+
+   
+}
---
+++ test2/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+namespace test2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public MainWindow(IHttpClientFactory httpClientFactory)
+        {
+            InitializeComponent();
+
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var client = httpClientFactory.CreateClient();
+            var response = await client.GetStringAsync
+                                 ("https://marcominerva.wordpress.com/feed/");
+            daLabel.Content = response;
+        }
+
+        
+    }
+
+}
